c94676b8d29cf778a63cfeae608a3f434eebf8f6
meil <14835841+ikonopistsev@users.noreply.github.com>
2020-05-11 17:21:39 +0300

    my_bool define

--- src/mysql.hpp
+++ src/mysql.hpp
@@ -1,7 +1,9 @@
 #pragma once
 
-#include "mysql.h"
+#include <mysql.h>
 
-#if MYSQL_VERSION_ID >= 80010
+// FIX my_bool was removed in 8.0.1
+#ifndef HAVE_TYPE_MY_BOOL
+#include <stdbool.h>
 typedef bool my_bool;
 #endif

35a02b610c8b35f398a822989542f0e3cfd67eae
meil <14835841+ikonopistsev@users.noreply.github.com>
2022-06-22 11:43:23 +0300

    v1.2.1

---
+++ build/CMakeFiles/capjs.dir/DependInfo.cmake
@@ -0,0 +1,25 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/meil/devel/linux/captor-meta/capjs/src/journal.cpp" "CMakeFiles/capjs.dir/src/journal.cpp.o" "gcc" "CMakeFiles/capjs.dir/src/journal.cpp.o.d"
+  "/home/meil/devel/linux/captor-meta/capjs/src/json_array.cpp" "CMakeFiles/capjs.dir/src/json_array.cpp.o" "gcc" "CMakeFiles/capjs.dir/src/json_array.cpp.o.d"
+  "/home/meil/devel/linux/captor-meta/capjs/src/json_date.cpp" "CMakeFiles/capjs.dir/src/json_date.cpp.o" "gcc" "CMakeFiles/capjs.dir/src/json_date.cpp.o.d"
+  "/home/meil/devel/linux/captor-meta/capjs/src/json_object.cpp" "CMakeFiles/capjs.dir/src/json_object.cpp.o" "gcc" "CMakeFiles/capjs.dir/src/json_object.cpp.o.d"
+  "/home/meil/devel/linux/captor-meta/capjs/src/json_time.cpp" "CMakeFiles/capjs.dir/src/json_time.cpp.o" "gcc" "CMakeFiles/capjs.dir/src/json_time.cpp.o.d"
+  "/home/meil/devel/linux/captor-meta/capjs/src/json_write.cpp" "CMakeFiles/capjs.dir/src/json_write.cpp.o" "gcc" "CMakeFiles/capjs.dir/src/json_write.cpp.o.d"
+  "/home/meil/devel/linux/captor-meta/capjs/src/mkkv.cpp" "CMakeFiles/capjs.dir/src/mkkv.cpp.o" "gcc" "CMakeFiles/capjs.dir/src/mkkv.cpp.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
